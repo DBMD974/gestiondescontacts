994f2f78f3efadf170686e92d20448145c8ce745
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-13 11:28:48 +0400

    rajout de news elements

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 
-
+<br>
 <header>
   <nav>
     <div class="logo">
@@ -27,6 +27,49 @@
     </div>
   </nav>
 </header>
+
+<section>
+<table>
+  <thead>
+    <tr>
+      <th>Nom</th>
+      <th>Prénom</th>
+      <th>Email</th>
+      <th>Téléphone</th>
+      <th>Entreprise</th>
+      <th>Adresse</th>
+      <th>Ville</th>
+      <th>Code Postal</th>
+      <th>Pays</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            ?>
+            <tr>
+              <td><?php echo $row['nom']; ?></td>
+              <td><?php echo $row['prenom']; ?></td>
+              <td><?php echo $row['email']; ?></td>
+              <td><?php echo $row['telephone']; ?></td>
+              <td><?php echo $row['entreprise']; ?></td>
+              <td><?php echo $row['adresse']; ?></td>
+              <td><?php echo $row['ville']; ?></td>
+              <td><?php echo $row['code_postal']; ?></td>
+              <td><?php echo $row['pays']; ?></td>
+            </tr>
+        <?php }
+    } else {
+        echo "<tr><td colspan='9'>Aucun contact trouvé.</td></tr>";
+    } ?>
+  </tbody>
+</table>
+
+<?php
+// Fermeture de la connexion à la base de données
+$conn->close();
+?>
+</section>
 
 
     
@@ -54,8 +97,18 @@
 
 
 
+<br>
 
-
+<footer>
+  <div class="container">
+    <p>&copy; 2023 Nom de votre entreprise. Tous droits réservés.</p>
+    <ul>
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#">À propos</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</footer>
 
 
 
